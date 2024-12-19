b6ec8c3d8328b0b23a2277caa2181974eb96dd4c
intheblackworld <intheblackworld@gmail.com>
2024-12-20 00:08:40 +0800

    modify

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function() {
     $(function () {
         // 設定目標日期為 2024年12月3日
-        var austDay = new Date(2024, 11, 3); // 注意：月份是從0開始計算，所以12月是11
+        var austDay = new Date(2025, 6, 25); // 注意：月份是從0開始計算，所以12月是11
         $('#defaultCountdown').countdown({
             until: austDay,
             format: 'DHMS',
